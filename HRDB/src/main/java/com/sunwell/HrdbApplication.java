3cfc90c5c7125babd85dc3d96bc21096d38b82c0
ShreeLakshmiA <63707256+ShreeLakshmiA@users.noreply.github.com>
2020-04-15 14:27:32 +0530

    HRDB project structure

---
+++ HRDB/src/main/java/com/sunwell/HrdbApplication.java
@@ -0,0 +1,13 @@
+package com.sunwell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrdbApplication.class, args);
+	}
+
+}
